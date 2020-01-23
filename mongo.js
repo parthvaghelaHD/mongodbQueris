f775650dbc5e82796edc714b5709a93951a49c5e
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-23 14:36:29 +0530

    find all Queries in mongo

--- mongo.js
+++ mongo.js
@@ -54,3 +54,35 @@ db.movies.insertMany([
 
 // to find every documents in  
 db.movies.find().pretty();
+
+// get all documents with writer set to "Quentin Tarantino"
+db.movies.find({"writer": "Quentin Tarantino"}).pretty();
+
+// get all documents where actors include "Brad Pitt"
+db.movies.find({"actors": "Brad Pitt"}).pretty()
+
+// get all documents with franchise set to "The Hobbit"
+db.movies.find({"franchise": "The Hobbit"})
+
+// get all movies released in the 90s
+db.movies.find({"year" : {$lte : 2000}}).pretty()
+
+// get all movies released before the year 2000 or after 2010
+db.movies.find({$or : [{"year" : {$lte : 2000}}, {"year" : {$gt: 2010}}] }).pretty()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
